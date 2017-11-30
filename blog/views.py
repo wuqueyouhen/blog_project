9a56a9264a37532a4d551c6befbf27c85abf95dd
liupeng <liupeng1011@foxmail.com>
2017-11-30 18:14:57 +0800

    1

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# 引用日志模块
+import logging
+
 from django.shortcuts import render
+from django.conf import settings
+
+
+logger = logging.getLogger('blog.views')
+
 
 # Create your views here.
+# 创建通用方法，调用settings.py中的定义的字段。注册本方法到settings.py中context_processors
+# 需要一个参数，如果不加会报错
+def global_setting(request):
+    return {
+        'SITE_NAME': settings.SITE_NAME,
+        'SITE_DESC': settings.SITE_DESC,
+    }
 
 
 def index(request):
+    try:
+        # 打开文件，并不存在，触发异常
+        file = open('aaa.txt', 'r')
+    except Exception as e:
+        # 讲异常写入log/error.log
+        logger.error(e)
     return render(request, 'index.html', locals())
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# 配置字符集，否则不能出现中文，注释中也不可以
 """
 Django settings for blog_project project.
 
@@ -64,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blog.views.global_setting',
             ],
         },
     },
@@ -120,7 +123,94 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 配置静态目录，注意[]
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+
+# 网站基本信息配置
+SITE_NAME = 'Python博客'
+SITE_DESC = '分享Python开发博客'
+
+
+
+# 自定义日志输出
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(threadName)s:%(thread)d] [%(name)s:%(lineno)d] [%(module)s:%(funcName)s] [%(levelname)s]- %(message)s'}  #日志格式
+    },
+    'filters': {
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+            },
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'log/all.log',     #日志输出文件
+            'maxBytes': 1024*1024*5,                  #文件大小
+            'backupCount': 5,                         #备份份数
+            'formatter': 'standard',                   #使用哪种formatters日志格式
+        },
+        'error': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'log/error.log',
+            'maxBytes': 1024*1024*5,
+            'backupCount': 5,
+            'formatter': 'standard',
+            },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'request_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'log/script.log',
+            'maxBytes': 1024*1024*5,
+            'backupCount': 5,
+            'formatter': 'standard',
+            },
+        'scprits_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'log/script.log',
+            'maxBytes': 1024*1024*5,
+            'backupCount': 5,
+            'formatter': 'standard',
+            }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['default', 'console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'django.request': {
+            'handlers': ['request_handler'],
+            'level': 'DEBUG',
+            'propagate': False,
+            },
+        'scripts': {
+            'handlers': ['scprits_handler'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'blog.views': {
+            'handlers': ['default', 'error'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+}
 
